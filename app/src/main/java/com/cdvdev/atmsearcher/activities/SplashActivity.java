3a43846d27630332759c1837d2249a36c44946c6
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2015-11-01 01:07:50 +0200

    Implement splash screen #AT-18 Done

---
+++ app/src/main/java/com/cdvdev/atmsearcher/activities/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.cdvdev.atmsearcher.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cdvdev.atmsearcher.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },
+                1000
+        );
+    }
+}
