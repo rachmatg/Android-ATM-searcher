1f93609f860d1f36e1eec96e84c7052737849e5c
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2016-01-02 17:04:51 +0200

    Implemented ViewHolder pattern for best performance #AT-20

--- app/src/main/java/com/cdvdev/atmsearcher/adapters/AtmListAdapter.java
+++ app/src/main/java/com/cdvdev/atmsearcher/adapters/AtmListAdapter.java
@@ -1,14 +1,15 @@
 package com.cdvdev.atmsearcher.adapters;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cdvdev.atmsearcher.App;
@@ -37,41 +38,48 @@ public class AtmListAdapter extends ArrayAdapter<Atm> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        if (convertView == null) {
-            convertView = ((Activity)mContext).getLayoutInflater().inflate(R.layout.item_atmlist, null);
-        }
-
         final Atm atm = getItem(position);
+        ViewHolder viewHolder;
 
-        TextView bankName = (TextView) convertView.findViewById(R.id.bank_name);
-        if (bankName != null) {
-            bankName.setText(atm.getBankName());
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = ((Activity)mContext).getLayoutInflater().inflate(R.layout.item_atmlist, null);
+            viewHolder.bankName = (TextView) convertView.findViewById(R.id.bank_name);
+            viewHolder.atmAddress = (TextView) convertView.findViewById(R.id.atm_address);
+            viewHolder.atmCity =  (TextView) convertView.findViewById(R.id.atm_city);
+            viewHolder.atmDistance = (TextView) convertView.findViewById(R.id.atm_distance);
+            viewHolder.layoutShowPlace = (FrameLayout) convertView.findViewById(R.id.ic_place_container);
+            viewHolder.layoutDistance = (LinearLayout) convertView.findViewById(R.id.layout_distance);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView atmAddress = (TextView) convertView.findViewById(R.id.atm_address);
-        if (atmAddress != null) {
-            atmAddress.setText(atm.getAddress());
+
+        if (viewHolder.bankName != null) {
+            viewHolder.bankName.setText(atm.getBankName());
         }
 
-        TextView atmCity = (TextView) convertView.findViewById(R.id.atm_city);
-        if (atmCity != null) {
-            atmCity.setText(atm.getCity());
+        if (viewHolder.atmAddress != null) {
+            viewHolder.atmAddress.setText(atm.getAddress());
         }
 
-        TextView atmDistance = (TextView) convertView.findViewById(R.id.atm_distance);
-        if (atmDistance != null) {
+        if (viewHolder.atmCity != null) {
+            viewHolder.atmCity.setText(atm.getCity());
+        }
+
+        if (viewHolder.atmDistance != null) {
             double distance = new BigDecimal(atm.getDistance()).setScale(3, RoundingMode.UP).doubleValue();
             if (distance > 0) {
                 String distanceText =  Double.toString(atm.getDistance());
-                atmDistance.setText(distanceText);
-                convertView.findViewById(R.id.layout_distance).setVisibility(View.VISIBLE);
+                viewHolder.atmDistance.setText(distanceText);
+                viewHolder.layoutDistance.setVisibility(View.VISIBLE);
             }
         }
 
 
-        FrameLayout showPlace = (FrameLayout) convertView.findViewById(R.id.ic_place_container);
-        if (showPlace != null) {
-            showPlace.setOnClickListener(new View.OnClickListener() {
+        if (viewHolder.layoutShowPlace != null) {
+            viewHolder.layoutShowPlace.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     FragmentManager fm =((FragmentActivity)mContext).getSupportFragmentManager();
@@ -98,5 +106,11 @@ public class AtmListAdapter extends ArrayAdapter<Atm> {
 
 
         return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView bankName, atmAddress, atmCity, atmDistance;
+        FrameLayout layoutShowPlace;
+        LinearLayout layoutDistance;
     }
 }
